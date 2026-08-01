aae4d87b71eed796d40bc0abb3df62a1c0257c66
deps4k <deps4k123@gmail.com>
2026-08-01 11:27:16 +0700

    Create wali-kelas.js

---
+++ wali-kelas.js
@@ -0,0 +1,30 @@
+// wali-kelas.js - Riwayat wali kelas PPLG 1
+const WALI_KELAS_HISTORY = [
+    {
+        tahun: "2024/2025",
+        kelas: "X PPLG 1",
+        nama: "Lusi Unsrimiati S.Pd",
+        gender: "female",
+        isActive: false
+    },
+    {
+        tahun: "2025/2026",
+        kelas: "XI PPLG 1",
+        nama: "Idit Rudianto S.Pd",
+        gender: "male",
+        isActive: false
+    },
+    {
+        tahun: "2026/2027",
+        kelas: "XII PPLG 1",
+        nama: "Panji Azwar Pebriana S.Pd",
+        gender: "male",
+        isActive: true
+    }
+];
+
+const WALI_KELAS_CONFIG = {
+    title: "Riwayat Wali Kelas PPLG 1",
+    icon: "fa-chalkboard-teacher",
+    description: "Daftar wali kelas yang pernah mengajar di PPLG 1 dari tahun ke tahun"
+};
