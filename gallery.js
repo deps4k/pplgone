40b1a8b50a0b6c6568c68f493a3410bcd6904414
deps4k <deps4k123@gmail.com>
2026-09-13 08:27:56 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,12 +1,12 @@
 // gallery.js - Data galeri dokumentasi
-const GALLERY_IMAGES = Array.from({ length: 87 }, (_, i) => ({
+const GALLERY_IMAGES = Array.from({ length: 95 }, (_, i) => ({
     file: `Galeri${i + 1}.jpg`,
     caption: `Kegiatan ${i + 1}`,
-    category: i < 30 ? "Pembelajaran" : i < 60 ? "Kegiatan Sekolah" : "Acara Kelas"
+    category: i < 30 ? "Kegiatan Sekolah" : i < 60 ? "Kegiatan Sekolah" : "Kegiatan Sekolah"
 }));
 
 const GALLERY_CONFIG = {
-    totalImages: 82,
+    totalImages: 95,
     basePath: "Assets/img/galeri/",
     perPage: 12
 }; 
